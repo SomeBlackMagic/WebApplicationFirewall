4e061100c4033248628b0250dfe4cc13c7cdf09d
Andru Cherny <8457572+wirwolf@users.noreply.github.com>
2025-03-27 18:23:47 +0200

    Enhance WAF Middleware and implement whitelist (#6)
    Refactor `WAFMiddleware` to include IP detection, GeoIP details, and whitelist checks. Add GitHub release workflow and extend Jest coverage reports. Update dependencies, TypeScript paths, and ensure proper module handling.

---
+++ src/Whitelist.ts
@@ -0,0 +1,100 @@
+
+import * as ipLib from 'ip';
+import {LoggerInterface} from "@elementary-lab/standards/src/LoggerInterface";
+import {Log} from "@waf/Log";
+
+export class Whitelist {
+    static #instance: Whitelist;
+
+    public static build(...args: [any, ...any[]]) {
+        Whitelist.instance = new Whitelist(...args);
+    }
+
+    public static get instance(): Whitelist {
+        return Whitelist.#instance;
+    }
+
+    public static set instance(obj: Whitelist) {
+        if(!Whitelist.#instance) {
+            Whitelist.#instance = obj;
+            return;
+        }
+
+        throw new Error('JailManager is already instantiated.');
+    }
+
+
+    public constructor(
+        private readonly config: IWhitelistConfig,
+        private readonly log?: LoggerInterface,
+    ) {
+        if(!log) {
+            this.log = Log.instance.withCategory('app.Whitelist');
+        }
+    }
+
+    public check(clientIp: string, clientGeoCountry: string, clientGeoCity: string): boolean {
+        if (this.config.ips && this.config.ips.length > 0) {
+            if(this.validateIncludeIPS(clientIp)) {
+                this.log.trace('Request from whitelisted IP', [clientIp]);
+                return true;
+            }
+        }
+
+        if (this.config.ipSubnet && this.config.ipSubnet.length > 0) {
+            if(this.validateIncludeIPSubnet(clientIp)) {
+                this.log.trace('Request from whitelisted IP subnet', [clientIp]);
+                return true;
+            }
+        }
+
+        if (this.config.geoCountry && this.config.geoCountry.length > 0) {
+            if(this.validateCountry(clientGeoCountry)) {
+                this.log.trace('Request from whitelisted country', [clientGeoCountry]);
+                return true;
+            }
+        }
+
+        if (this.config.geoCity && this.config.geoCity.length > 0) {
+            if(this.validateCity(clientGeoCity)) {
+                this.log.trace('Request from whitelisted city', [clientGeoCity]);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private validateIncludeIPS(ip: string): boolean {
+        return this.config.ips.includes(ip);
+    }
+
+    private validateIncludeIPSubnet(ip: string): boolean {
+        return this.config.ipSubnet.some(subnet => {
+            return ipLib.cidrSubnet(subnet).contains(ip)
+        });
+    }
+
+
+    private validateCountry(country: string): boolean {
+        return this.config.geoCountry.some(countryName => {
+            return countryName == country
+        });
+    }
+
+    private validateCity(city: string): boolean {
+        return this.config.geoCity.some(cityItem => {
+            return city === cityItem
+        });
+    }
+
+
+
+}
+
+export interface IWhitelistConfig {
+    ips?: string[],
+    ipSubnet?: string[],
+    geoCountry?: string[],
+    geoCity?: string[]
+}
--- test/unit/WAFMiddleware.test.ts
+++ test/unit/WAFMiddleware.test.ts
@@ -1,195 +1,206 @@
 import {Request, Response} from 'express';
-import {WAFMiddleware} from "../../src/WAFMiddleware";
-import {JailManager} from "../../src/Jail/JailManager";
-import { IAbstractRuleConfig} from "../../src/Rules/AbstractRule";
+import {FlexibleRule} from "@waf/Rules/FlexibleRule";
+import {JailManager} from "@waf/Jail/JailManager";
+import {WAFMiddleware} from "@waf/WAFMiddleware";
+import {CompositeRule} from "@waf/Rules/CompositeRule";
+import {StaticRule} from "@waf/Rules/StaticRule";
+import {IAbstractRuleConfig} from "@waf/Rules/AbstractRule";
 // @ts-ignore
-import {DummyRule} from "../Helpers/DummyRule";
-import {CompositeRule, ICompositeRuleConfig} from "../../src/Rules/CompositeRule";
-import {IStaticRuleConfig, StaticRule} from "../../src/Rules/StaticRule";
-import {FlexibleRule, IFlexibleRuleConfig} from "../../src/Rules/FlexibleRule";
+import {DummyRule} from "@test/Helpers/DummyRule";
+import {Whitelist} from "@waf/Whitelist";
+import {MockRequest} from 'node-mocks-http';
 
-describe('WAFMiddleware.wafMiddleware', () => {
+
+describe('WAFMiddleware', () => {
     jest.useFakeTimers();
     jest.spyOn(global, 'setInterval');
 
-    it('should call next() when the client IP is not blocked and all rules pass', async () => {
-        expect.assertions(1);
+    describe('use', () => {
+        jest.useFakeTimers();
+        jest.spyOn(global, 'setInterval');
 
-        const jailManager = new JailManager({});
-        const middleware = new WAFMiddleware(jailManager);
+        it('should call next() when the client IP is not blocked and all rules pass', async () => {
 
-        const next = jest.fn();
-        const req = <Request>{
-            headers: {}
-        };
-        const res = <Response>{};
+            const jailManager = new JailManager({});
+            const mockGetBlockedIp = jest.spyOn(jailManager, 'getBlockedIp').mockReturnValue(false);
 
-        await middleware.wafMiddleware()(req, res, next);
+            const whitelist = new Whitelist({});
+            const mockCheck = jest.spyOn(whitelist, 'check').mockReturnValue(false);
 
-        expect(next).toHaveBeenCalledTimes(1);
-    });
+            const middleware = new WAFMiddleware({detectClientIp: {headers: []}}, jailManager, whitelist);
 
-    it('should respond with a 429 status code when the client IP is blocked', async () => {
-        expect.assertions(1);
+            const next = jest.fn();
+            const req = <Request>{
+                headers: {}
+            };
+            const res = <Response>{};
 
-        const jailManager = new JailManager({});
-        //mock the response from JailManager
-        jailManager.getBlockedIp = jest.fn().mockReturnValueOnce({
-            unbanTime: Date.now() + 1000
+            await middleware.use()(req, res, next);
+
+            expect(mockCheck).toHaveBeenCalledTimes(1);
+            expect(mockGetBlockedIp).toHaveBeenCalledTimes(1);
+            expect(next).toHaveBeenCalledTimes(1);
         });
 
-        const middleware = new WAFMiddleware(jailManager);
+        it('should respond with a 429 status code when the client IP is blocked', async () => {
 
-        const req = <Request>{
-            headers: {}
-        };
-        const res = <Response><unknown>{
-            sendStatus: jest.fn().mockReturnThis(),
-        };
-        const next = jest.fn();
+            const jailManager = new JailManager({});
+            //mock the response from JailManager
+            jailManager.getBlockedIp = jest.fn().mockReturnValueOnce({
+                unbanTime: Date.now() + 1000
+            });
 
-        await middleware.wafMiddleware()(req, res, next);
+            const whitelist = new Whitelist({});
+            const mockCheck = jest.spyOn(whitelist, 'check').mockReturnValue(false);
 
-        expect(res.sendStatus).toHaveBeenCalledWith(429);
+            const middleware = new WAFMiddleware({detectClientIp: {headers: []}}, jailManager, whitelist);
+
+            const req = <Request>{
+                headers: {}
+            };
+            const res = <Response><unknown>{
+                sendStatus: jest.fn().mockReturnThis(),
+            };
+            const next = jest.fn();
+
+            await middleware.use()(req, res, next);
+
+            expect(mockCheck).toHaveBeenCalledTimes(1);
+            expect(res.sendStatus).toHaveBeenCalledWith(429);
+        });
+
+        it('should respond with a 429 status code when any rule fails', async () => {
+
+            const failingRule = new DummyRule(true);
+            failingRule.use = jest.fn().mockResolvedValueOnce(true);
+
+            const jailManager = new JailManager({});
+
+            const whitelist = new Whitelist({});
+            const mockCheck = jest.spyOn(whitelist, 'check').mockReturnValue(false);
+
+            const middleware = new WAFMiddleware({detectClientIp: {headers: []}}, jailManager, whitelist);
+            middleware['rules'] = [failingRule];
+
+            const req = <Request>{
+                headers: {}
+            };
+            const res = <Response><unknown>{
+                sendStatus: jest.fn().mockReturnThis(),
+            };
+            const next = jest.fn();
+
+            await middleware.use()(req, res, next);
+
+            expect(mockCheck).toHaveBeenCalledTimes(1);
+            expect(res.sendStatus).toHaveBeenCalledWith(429);
+        });
     });
 
-    it('should respond with a 429 status code when any rule fails', async () => {
-        expect.assertions(1);
+    describe('loadRules', () => {
+        let service;
 
-        const failingRule = new DummyRule(true);
-        failingRule.use = jest.fn().mockResolvedValueOnce(true);
+        beforeEach(() => {
+            service = new WAFMiddleware({detectClientIp: {headers: []}});
+        });
 
+        it('should load all types of rules', () => {
+            jest.mock('../../src/Rules/StaticRule')
+            // @ts-ignore
+            const compositeRuleConfig: ICompositeRuleConfig = {
+                type: CompositeRule.ID,
+                // add other properties as required
+            };
+            // @ts-ignore
+            const staticRuleConfig: IStaticRuleConfig = {
+                type: StaticRule.ID,
+                // add other properties as required
+            };
+            // @ts-ignore
+            const flexibleRuleConfig: IFlexibleRuleConfig = {
+                type: FlexibleRule.ID,
+                // add other properties as required
+            };
+
+            const rulesConfig: IAbstractRuleConfig[] = [
+                compositeRuleConfig,
+                staticRuleConfig,
+                flexibleRuleConfig
+            ];
+
+            const result = service.loadRules(rulesConfig);
+
+            expect(result).toBe(true);
+            expect(service['rules'].length).toBe(3);
+            expect(service['rules'][0] instanceof CompositeRule).toBe(true);
+            expect(service['rules'][1] instanceof StaticRule).toBe(true);
+            expect(service['rules'][2] instanceof FlexibleRule).toBe(true);
+        });
+
+        it('should throw when an invalid rule type is provided', () => {
+            const invalidRule: IAbstractRuleConfig = {type: 'Invalid'};
+
+            const rulesConfig: IAbstractRuleConfig[] = [invalidRule];
+
+            expect(() => {
+                service.loadRules(rulesConfig);
+            }).toThrow('Can not found observer or rule type - Invalid');
+        });
+    });
+
+    describe('detectClientIp', () => {
         const jailManager = new JailManager({});
-        const middleware = new WAFMiddleware(jailManager);
-        middleware['rules'] = [failingRule];
+        const whitelist = new Whitelist({});
 
-        const req = <Request>{
-            headers: {}
-        };
-        const res = <Response><unknown>{
-            sendStatus: jest.fn().mockReturnThis(),
-        };
-        const next = jest.fn();
+        const middleware = new WAFMiddleware({detectClientIp: {headers: ['x-real-ip']}}, jailManager, whitelist);
 
-        await middleware.wafMiddleware()(req, res, next);
 
-        expect(res.sendStatus).toHaveBeenCalledWith(429);
-    });
-});
+        it('should return the IP from headers specified in config', () => {
+            const req = {
+                headers: {
+                    'x-real-ip': '192.168.1.1',
+                },
+                ip: '127.0.0.1',
+            };
+            const clientIp = middleware.detectClientIp(req as unknown as Request);
+            expect(clientIp).toBe('192.168.1.1');
+        });
 
-describe('WAFMiddleware.getClientIp', () => {
-    let service;
+        it('should return the first IP from x-forwarded-for header', () => {
+            const req = {
+                headers: {
+                    'x-forwarded-for': '192.168.1.2, 192.168.1.3',
+                },
+                ip: '127.0.0.1',
+            };
 
-    beforeEach(() => {
-        service = new WAFMiddleware();
-    });
+            const clientIp = middleware.detectClientIp(req as unknown as Request);
+            expect(clientIp).toBe('192.168.1.2');
+        });
 
-    test('should return fastly-client-ip if it is defined', () => {
-        const req = {
-            headers: {
-                'fastly-client-ip': '1.1.1.1'
-            },
-            ip: '127.0.0.1'
-        };
+        it('should return req.ip when no relevant headers are present', () => {
+            const req = <Request>{
+                headers: {},
+                ip: '127.0.0.1',
+            };
 
-        const ip = service.getClientIp(req);
-        expect(ip).toBe('1.1.1.1');
-    });
+            const clientIp = middleware.detectClientIp(req);
+            expect(clientIp).toBe('127.0.0.1');
+        });
 
-    test('should return cf-connecting-ip if fastly-client-ip is not defined', () => {
-        const req = {
-            headers: {
-                'cf-connecting-ip': '2.2.2.2'
-            },
-            ip: '127.0.0.1'
-        };
+        it('should return undefined when x-forwarded-for is present but empty', () => {
+            const req = {
+                headers: {
+                    'x-forwarded-for': '',
+                },
+                ip: '127.0.0.1',
+            };
 
-        const ip = service.getClientIp(req);
-        expect(ip).toBe('2.2.2.2');
-    });
-
-    test('should return x-original-forwarded-for if the first two headers are not defined', () => {
-        const req = {
-            headers: {
-                'x-original-forwarded-for': '3.3.3.3'
-            },
-            ip: '127.0.0.1'
-        };
-
-        const ip = service.getClientIp(req);
-        expect(ip).toBe('3.3.3.3');
+            const clientIp = middleware.detectClientIp(req as unknown as Request);
+            expect(clientIp).toBe('127.0.0.1');
+        });
     });
 
-    test('should return the first IP from x-forwarded-for header when it is defined', () => {
-        const req = {
-            headers: {
-                'x-forwarded-for': '4.4.4.4, 5.5.5.5'
-            },
-            ip: '127.0.0.1'
-        };
 
-        const ip = service.getClientIp(req);
-        expect(ip).toBe('4.4.4.4');
-    });
+})
 
-    test('should return req.ip if none of the headers are defined', () => {
-        const req = {
-            headers: {},
-            ip: '127.0.0.1'
-        };
-
-        const ip = service.getClientIp(req);
-        expect(ip).toBe('127.0.0.1');
-    });
-});
-
-describe('WAFMiddleware.loadRules', () => {
-    let service;
-
-    beforeEach(() => {
-        service = new WAFMiddleware();
-    });
-
-    it('should load all types of rules', () => {
-        jest.mock('../../src/Rules/StaticRule')
-        // @ts-ignore
-        const compositeRuleConfig: ICompositeRuleConfig = {
-            type: CompositeRule.ID,
-            // add other properties as required
-        };
-        // @ts-ignore
-        const staticRuleConfig: IStaticRuleConfig = {
-            type: StaticRule.ID,
-            // add other properties as required
-        };
-        // @ts-ignore
-        const flexibleRuleConfig: IFlexibleRuleConfig = {
-            type: FlexibleRule.ID,
-            // add other properties as required
-        };
-
-        const rulesConfig: IAbstractRuleConfig[] = [
-            compositeRuleConfig,
-            staticRuleConfig,
-            flexibleRuleConfig
-        ];
-
-        const result = service.loadRules(rulesConfig);
-
-        expect(result).toBe(true);
-        expect(service['rules'].length).toBe(3);
-        expect(service['rules'][0] instanceof CompositeRule).toBe(true);
-        expect(service['rules'][1] instanceof StaticRule).toBe(true);
-        expect(service['rules'][2] instanceof FlexibleRule).toBe(true);
-    });
-
-    it('should throw when an invalid rule type is provided', () => {
-        const invalidRule: IAbstractRuleConfig = {type: 'Invalid'};
-
-        const rulesConfig: IAbstractRuleConfig[] = [invalidRule];
-
-        expect(() => {
-            service.loadRules(rulesConfig);
-        }).toThrow('Can not found observer or rule type - Invalid');
-    });
-});
